75aa0c16ef8357ccdf7b0166797e806e25a6717a
Ashith Bhandary <68805894+ashith-cloud@users.noreply.github.com>
2022-03-02 11:19:14 +1100

    changes in backend url

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pre_process
 import post_process
 import model
 import os, shutil
+import time
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,39 +13,45 @@ api = Api(app)
 class predict(Resource):
     @staticmethod
     def post():
-        # try:
-        # Loads the body of the event.
-        input_dict = request.get_json()
-        inputdata = input_dict["dataFileURL"]
+        try:
+            start = time.time();
+            # Loads the body of the event.
+            input_dict = request.get_json()  
+            backend_url = input_dict["datashopServerAddress"] 
 
-        if(os.path.exists("tmp")):
-            shutil.rmtree("tmp")
-        os.mkdir('tmp')
+            if(os.environ["BACKEND_URL"] != backend_url):
+                os.environ["BACKEND_URL"] = backend_url
+        
+            inputdata = input_dict["dataFileURL"]        
+            if(os.path.exists("tmp")):
+                shutil.rmtree("tmp")
+            os.mkdir('tmp')
+            #updating the datashop job as running
+            post_process.updateJob(input_dict["jobID"], "running", None)
 
-        #updating the datashop job as running
-        post_process.updateJob(input_dict["jobID"], "running", None)
+            # running the preprocessing steps for the model. It takes dataset URL, jobID, json as input, download the dataset and read the input.
+            inputPayloadForService = pre_process.run(input_dict["jobID"], inputdata["url"], inputdata["json"])
+            
+            # model buliding/ getting the predictions here. It takes jobID and inputPayloadForService as input, run the model and get precitions saved in the temp folder of lambda.
+            insightsDataFileLocation = model.run(input_dict["jobID"], inputPayloadForService)
 
-        # running the preprocessing steps for the model. It takes dataset URL, jobID, json as input, download the dataset and read the input.
-        inputPayloadForService = pre_process.run(input_dict["jobID"], inputdata["url"], inputdata["json"])
+            # It takes insightsDataFileLocation, jobID as Input, upload the insights file to s3 and get the downloadable link for the same. and also send the jobID and insights link to the Datashop application.
+            status_map = post_process.run(input_dict["jobID"], insightsDataFileLocation)
+            print(status_map)
 
-        # model buliding/ getting the predictions here. It takes jobID and inputPayloadForService as input, run the model and get precitions saved in the temp folder of lambda.
-        insightsDataFileLocation = model.run(input_dict["jobID"], inputPayloadForService)
+            duration = time.time() - start;
 
-        # It takes insightsDataFileLocation, jobID as Input, upload the insights file to s3 and get the downloadable link for the same. and also send the jobID and insights link to the Datashop application.
-        status_map = post_process.run(input_dict["jobID"], insightsDataFileLocation)
-        print(status_map)
-        return {"statusCode": status_map["status_code"], "body": status_map["json_response"]}
-
-        # except Exception as e:
-        #     #updating job with FAILED status.
-        #     try:
-        #         post_process.updateJob(input_dict["jobID"], None, str(e))
-        #         return {"statusCode": 400, "error": str(e)}
-        #     except Exception as e:
-        #         return {"statusCode": 400, "error": str(e)}
-        #
-
-
+            return {"statusCode": status_map["status_code"], "body": status_map["json_response"], "duration":duration}
+                        
+        except Exception as e:
+            #updating job with FAILED status.
+            try:
+                duration = time.time() - start;
+                post_process.updateJob(input_dict["jobID"], None, str(e))
+                return {"statusCode": 400, "error": str(e), "duration":duration}
+            except Exception as e:
+                duration = time.time() - start;
+                return {"statusCode": 400, "error": str(e),"duration":duration}                
 
 api.add_resource(predict, '/predict')
 
